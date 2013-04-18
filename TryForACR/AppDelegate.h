c7632fc634a61bdbad198c4975e08d2adfd70a82
youngyun <412794844@qq.com>
2013-04-18 15:21:14 +0800

    Initial Commit

---
+++ TryForACR/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TryForACR
+//
+//  Created by apple on 13-4-18.
+//  Copyright (c) 2013年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
